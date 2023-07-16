2a027224d8ec695d76108e2e0054d293b62d3324
Mahmoud Hassan <mahmoud.hassan.ahmed3@gmail.com>
2023-07-17 02:25:28 +0300

    merge tasks

--- src/app/schools-module/schools-module-routing.module.ts
+++ src/app/schools-module/schools-module-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SchoolsLayoutComponent } from './Components/schools-layout/schools-layout.component';
 import { SchoolsHomePageComponent } from './Components/schools-home-page/schools-home-page.component';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { ReportTripsComponent } from './Components/report-trips/report-trips.component';
 
 const routes: Routes = [
   {path:'',component:SchoolsLayoutComponent,children:[
     {path:'',component:SchoolsHomePageComponent}
-  ]}
+  ]},
+  {path:'profile',component:ProfileComponent},
+  {path:'trip-reports',component:ReportTripsComponent}
 ];
 
 @NgModule({
--- src/app/schools-module/schools-module.module.ts
+++ src/app/schools-module/schools-module.module.ts
@@ -18,6 +18,8 @@ import { NewPasswordComponent } from './Components/new-password/new-password.com
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { ActivitiesComponent } from './Components/activities/activities.component';
 import { GroupsComponent } from './Components/groups/groups.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { ReportTripsComponent } from './Components/report-trips/report-trips.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +33,8 @@ import { GroupsComponent } from './Components/groups/groups.component';
     HomePageComponent,
     ActivitiesComponent,
     GroupsComponent,
+    ProfileComponent,
+    ReportTripsComponent,
   ],
   imports: [
     CommonModule,
