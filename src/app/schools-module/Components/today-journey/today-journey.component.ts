e45a7c50a4ee520890ef485831039242129274f7
Mahmoud-Abdelmoughiss <abdelmoughiss@gmail.com>
2023-07-18 15:21:31 +0300

    continue editing journey tracking

--- src/app/schools-module/Components/today-journey/today-journey.component.ts
+++ src/app/schools-module/Components/today-journey/today-journey.component.ts
@@ -9,6 +9,8 @@ export class TodayJourneyComponent implements OnInit {
   allJournies:boolean = true;
   returnJourney:boolean = false;
   outboundJourney:boolean = false;
+  isAllJourneySelected:boolean=false;
+  isActiveJourneySelected:boolean=false;
 
   journies:any[] = [];
   journey_data = [
@@ -44,14 +46,34 @@ export class TodayJourneyComponent implements OnInit {
     this.outboundJourney = true;
   }
   todayJournies=[
-    {label:'إجمالي عدد الطلاب : 30',activityName:'طلبات الغياب',count:'9',percent:'15'},
-    {label:'إجمالي عدد الرحلات : 30',activityName:'عدد الرحلات',count:'28',percent:'70'},
-    {label:'إجمالي عدد الرحلات اليوم : 30',activityName:'الرحلات الفعالة',count:'س',percent:'58'},
+    {type:'all',label:'إجمالي عدد الرحلات : ',activityName:'عدد الرحلات',count:'28',percent:'70'},
+    {type:'active',label:' عدد الرحلات الفعالة الآن : ',activityName:'طلبات الغياب',count:'9',percent:'15'},
   ];
   strokArray:any[] = this.todayJournies.map(val=>`${val.percent}, 100`);
-
+  
+  groups=[
+    {id:1,name:'المجموعة الاولى',studentCount:19},
+    {id:3,name:'المجموعة الثانية',studentCount:13},
+    {id:2,name:'المجموعة الثالثة',studentCount:15},
+    {id:4,name:'المجموعة الرابعة',studentCount:8},
+    {id:5,name:'المجموعة الخامسة',studentCount:14},
+    {id:6,name:'المجموعة السادسة',studentCount:10},
+    {id:7,name:'المجموعة السابعة',studentCount:29},
+    {id:8,name:'المجموعة الثامنة',studentCount:39},
+    {id:9,name:'المجموعة التاسعه',studentCount:18},
+    {id:10,name:'المجموعة العاشرة',studentCount:20},
+  ];
   ngOnInit(): void {
     this.getAllJourney();
+  }
+  showJourney(type:any){
+    if(type=='all'){
+      this.isAllJourneySelected = true;
+      this.isActiveJourneySelected = false;
+    }else{
+      this.isActiveJourneySelected = true;
+      this.isAllJourneySelected = false;
+    }
   }
 
 }
